50052cb14838e7cf5dd1bcd1385bf3596a60fbf6
이한솔 <hansol8701@naver.com>
2023-01-04 15:38:27 +0900

    feat(Member) : API 스웨거 적용

---
+++ dabom-backend/src/main/java/joeuncamp/dabombackend/domain/member/MemberController.java
@@ -0,0 +1,25 @@
+package joeuncamp.dabombackend.domain.member;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "[Member]", description = "회원과 관련된 API입니다.")
+@RequestMapping("/members")
+public class MemberController {
+    @Operation(summary = "회원 생성", description = "회원을 생성합니다.")
+    @PostMapping("")
+    public void createMember(){
+
+    }
+
+    @Operation(summary = "전체 회원 조회", description = "전체 회원을 조회합니다.")
+    @GetMapping("")
+    public void getAllMember(){
+
+    }
+}
--- dabom-backend/src/main/java/joeuncamp/dabombackend/global/config/SpringDocConfig.java
+++ dabom-backend/src/main/java/joeuncamp/dabombackend/global/config/SpringDocConfig.java
@@ -3,8 +3,6 @@ package joeuncamp.dabombackend.global.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import org.springdoc.core.GroupedOpenApi;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
