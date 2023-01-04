d01504d0ffae551feb53eec4bfd00ed5962c0bfc
이한솔 <hansol8701@naver.com>
2023-01-04 11:37:30 +0900

    프로젝트 생성

---
+++ dabom-backend/src/test/java/joeuncamp/dabom/dabombackend/DabomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package joeuncamp.dabom.dabombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DabomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
