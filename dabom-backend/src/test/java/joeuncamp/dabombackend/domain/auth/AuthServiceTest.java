f4f208d4671c809d1c5dcc41e64fe68451b24c21
이한솔 <hansol8701@naver.com>
2023-01-10 13:18:02 +0900

    Test(Auth) : 기존 계정으로 회원가입 시 예외를 검증하는 테스트 코드 작성

--- dabom-backend/src/test/java/joeuncamp/dabombackend/domain/auth/AuthServiceTest.java
+++ dabom-backend/src/test/java/joeuncamp/dabombackend/domain/auth/AuthServiceTest.java
@@ -1,7 +1,9 @@
 package joeuncamp.dabombackend.domain.auth;
 
 import joeuncamp.dabombackend.domain.entity.Member;
+import joeuncamp.dabombackend.domain.member.dto.MemberCreationRequestDto;
 import joeuncamp.dabombackend.domain.member.repository.MemberJpaRepository;
+import joeuncamp.dabombackend.global.constant.ExampleValue;
 import joeuncamp.dabombackend.global.error.exception.CLoginFailedException;
 import joeuncamp.dabombackend.global.error.exception.CMemberExistException;
 import joeuncamp.dabombackend.global.security.jwt.JwtProvider;
@@ -19,6 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,7 +39,27 @@ public class AuthServiceTest {
     @Mock
     PasswordEncoder passwordEncoder;
 
+    @Test
+    @DisplayName("계정이 이미 존재하는 경우 회원가입이 실패한다.")
+    void 계정이_이미_존재하는_경우_회원가입이_실패한다() {
+        // given
+        Member existingMember = Member.builder()
+                .account(ExampleValue.Member.ACCOUNT)
+                .password(ExampleValue.Member.PASSWORD)
+                .build();
 
+        SignUpRequestDto dto = SignUpRequestDto.builder()
+                .account(ExampleValue.Member.ACCOUNT)
+                .password(ExampleValue.Member.PASSWORD)
+                .build();
+
+        given(memberJpaRepository.findByAccount(dto.getAccount())).willReturn(Optional.of(existingMember));
+        // when
+
+        // then
+        assertThatThrownBy(() -> authService.signup(dto))
+                .isInstanceOf(CMemberExistException.class);
+    }
 
 
     @Test
@@ -44,24 +67,23 @@ public class AuthServiceTest {
     void 비밀번호가_일치하지_않으면_로그인이_실패한다() {
         // given
         Member member = Member.builder()
-                .account("original_account")
-                .password("original_password")
+                .account(ExampleValue.Member.ACCOUNT)
+                .password(ExampleValue.Member.PASSWORD)
                 .build();
 
-        LoginRequestDto loginRequestDto = LoginRequestDto.builder()
-                .account("test_account")
-                .password("test_password")
+        LoginRequestDto dto = LoginRequestDto.builder()
+                .account(ExampleValue.Member.ACCOUNT)
+                .password("invalid_password")
                 .build();
 
-        when(memberJpaRepository.findByAccount(loginRequestDto.getAccount())).thenReturn(Optional.of(member));
-        when(passwordEncoder.matches(loginRequestDto.getPassword(), member.getPassword())).thenReturn(false);
+        when(memberJpaRepository.findByAccount(dto.getAccount())).thenReturn(Optional.of(member));
+        when(passwordEncoder.matches(dto.getPassword(), member.getPassword())).thenReturn(false);
 
         // when
 
         // then
-        assertThatThrownBy(
-                () -> authService.login(loginRequestDto)
-        ).isInstanceOf(CLoginFailedException.class);
+        assertThatThrownBy(() -> authService.login(dto))
+                .isInstanceOf(CLoginFailedException.class);
     }
 
 }
