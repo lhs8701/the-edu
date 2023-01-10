d86e4b672118d951f31eefd35ed5debd3959a369
이한솔 <hansol8701@naver.com>
2023-01-10 14:49:58 +0900

    사용하지 않는 클래스 정리

--- dabom-backend/src/main/java/joeuncamp/dabombackend/domain/member/MemberController.java
+++ dabom-backend/src/main/java/joeuncamp/dabombackend/domain/member/MemberController.java
@@ -23,19 +23,5 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final MemberService memberService;
 
-    @PreAuthorize("permitAll()")
-    @Operation(summary = "회원 생성", description = "회원을 생성합니다.")
-    @PostMapping("")
-    public void createMember(@Valid MemberCreationRequestDto requestDto) {
-        memberService.createMember(requestDto);
-    }
-
-    @Parameter(name = Header.JWT_HEADER, description = "AccessToken", required = true, in = ParameterIn.HEADER, example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQG5hdmVyLmNvbSIsImFjY291bnQiOiJ1c2VyQG5hdmVyLmNvbSIsImF1dGhvcml0aWVzIjoiUk9MRV9VU0VSIiwiaWF0IjoxNjczMzAzMzY5LCJleHAiOjE3MDQ4MzkzNjl9.s_HnINVN5QTNdmCNdRJ0XBx2jISxuvvywJB_tA6SA9Q")
-    @PreAuthorize("hasRole('USER')")
-    @Operation(summary = "회원 단일 조회", description = "회원을 한명을 조회합니다.")
-    @GetMapping("/{memberId}")
-    public ResponseEntity<Member> getMember(@PathVariable Long memberId) {
-        return new ResponseEntity<>(memberService.getMember(memberId), HttpStatus.OK);
-    }
 }
 
