d6c8a4c62a8acb13eb08d4dd5ab46627093cb424
이한솔 <hansol8701@naver.com>
2023-01-11 14:25:32 +0900

    docs(Member) : 주석 추가

--- dabom-backend/src/main/java/joeuncamp/dabombackend/domain/member/MemberController.java
+++ dabom-backend/src/main/java/joeuncamp/dabombackend/domain/member/MemberController.java
@@ -1,10 +1,12 @@
 package joeuncamp.dabombackend.domain.member;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import joeuncamp.dabombackend.domain.member.dto.ProfileResponseDto;
 import joeuncamp.dabombackend.domain.member.dto.ProfileUpdateParam;
+import joeuncamp.dabombackend.domain.member.entity.Member;
 import joeuncamp.dabombackend.domain.member.service.MemberService;
 import joeuncamp.dabombackend.global.constant.ExampleValue;
 import joeuncamp.dabombackend.global.constant.Header;
@@ -12,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -22,18 +25,20 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final MemberService memberService;
 
+    @Operation(summary = "회원 정보를 조회합니다.")
     @Parameter(name = Header.JWT_HEADER, description = "AccessToken", required = true, in = ParameterIn.HEADER, example = ExampleValue.JWT.ACCESS)
     @PreAuthorize("hasRole('USER')")
     @GetMapping("/{memberId}/profile")
-    public ResponseEntity<ProfileResponseDto> getMyProfile(@PathVariable Long memberId){
+    public ResponseEntity<ProfileResponseDto> getMyProfile(@PathVariable Long memberId) {
         return new ResponseEntity<>(memberService.getMyProfile(memberId), HttpStatus.OK);
     }
 
+    @Operation(summary = "자신의 회원 정보를 수정합니다.", description = "수정하지 않을 항목은 비워두면 됩니다.")
     @Parameter(name = Header.JWT_HEADER, description = "AccessToken", required = true, in = ParameterIn.HEADER, example = ExampleValue.JWT.ACCESS)
     @PreAuthorize("hasRole('USER')")
     @PatchMapping("/{memberId}/profile")
-    public ResponseEntity<Long> updateMyProfile(@RequestBody ProfileUpdateParam updateParam, @PathVariable Long memberId){
-        return new ResponseEntity<>(memberService.updateMyProfile(updateParam, memberId), HttpStatus.OK);
+    public ResponseEntity<Long> updateMyProfile(@AuthenticationPrincipal Member member, @RequestBody ProfileUpdateParam updateParam, @PathVariable Long memberId) {
+        return new ResponseEntity<>(memberService.updateMyProfile(updateParam, member.getId()), HttpStatus.OK);
     }
 }
 
--- dabom-backend/src/main/java/joeuncamp/dabombackend/domain/member/service/MemberService.java
+++ dabom-backend/src/main/java/joeuncamp/dabombackend/domain/member/service/MemberService.java
@@ -13,12 +13,25 @@ import org.springframework.stereotype.Service;
 public class MemberService {
     private final MemberJpaRepository memberRepository;
 
-    public ProfileResponseDto getMyProfile(Long memberId){
+    /**
+     * 나의 개인 정보를 조회합니다.
+     *
+     * @param memberId 조회할 회원의 아이디넘버
+     * @return 개인 정보
+     */
+    public ProfileResponseDto getMyProfile(Long memberId) {
         Member member = memberRepository.findById(memberId).orElseThrow(CResourceNotFoundException::new);
         return new ProfileResponseDto(member);
     }
 
-    public Long updateMyProfile(ProfileUpdateParam profileUpdateParam, Long memberId){
+    /**
+     * 나의 개인 정보를 수정합니다. 수정 인자 중 null값을 제외하고 반영합니다.
+     *
+     * @param profileUpdateParam 수정 인자
+     * @param memberId           수정할 회원의 아이디넘버
+     * @return 수정한 회원의 아이디넘버
+     */
+    public Long updateMyProfile(ProfileUpdateParam profileUpdateParam, Long memberId) {
         Member member = memberRepository.findById(memberId).orElseThrow(CResourceNotFoundException::new);
         member.updateProfile(profileUpdateParam);
         return member.getId();
