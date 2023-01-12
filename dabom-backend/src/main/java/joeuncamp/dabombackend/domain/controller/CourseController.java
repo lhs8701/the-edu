97bd24bc197107ac125ed4d37052cf4fd1aa6b8a
이한솔 <hansol8701@naver.com>
2023-01-12 10:39:36 +0900

    feat(Course) : 강좌 단건 조회 컨트롤러 api 구현

--- dabom-backend/src/main/java/joeuncamp/dabombackend/domain/controller/CourseController.java
+++ dabom-backend/src/main/java/joeuncamp/dabombackend/domain/controller/CourseController.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import joeuncamp.dabombackend.domain.course.dto.CourseCreationRequestDto;
+import joeuncamp.dabombackend.domain.course.dto.CourseResponseDto;
+import joeuncamp.dabombackend.domain.course.entity.Course;
 import joeuncamp.dabombackend.domain.course.service.CourseService;
 import joeuncamp.dabombackend.domain.member.entity.Member;
 import joeuncamp.dabombackend.global.constant.ExampleValue;
@@ -14,10 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "[Course]", description = "클래스와 관련된 API입니다.")
 @RestController
@@ -29,8 +28,18 @@ public class CourseController {
     @Operation(summary = "강좌를 개설합니다.", description = "강좌 개설은 크리에이터 프로필을 활성화한 회원만 가능합니다. \n 개설된 강좌의 아이디넘버가 반환됩니다.")
     @Parameter(name = Header.JWT_HEADER, description = "AccessToken", required = true, in = ParameterIn.HEADER, example = ExampleValue.JWT.ACCESS)
     @PreAuthorize("hasRole('USER')")
-    @PostMapping
+    @PostMapping("")
     public ResponseEntity<Long> openCourse(@AuthenticationPrincipal Member member, @RequestBody CourseCreationRequestDto dto){
         return new ResponseEntity<>(1L, HttpStatus.CREATED);
+    }
+
+
+    @Operation(summary="", description="")
+    @Parameter(name = Header.JWT_HEADER, description="어세스토큰", required=true, in=ParameterIn.HEADER, example=ExampleValue.JWT.ACCESS)
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/{courseId}")
+    public ResponseEntity<CourseResponseDto> getCourse(@PathVariable Long courseId){
+        CourseResponseDto responseDto = courseService.getCourse(courseId);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 }
