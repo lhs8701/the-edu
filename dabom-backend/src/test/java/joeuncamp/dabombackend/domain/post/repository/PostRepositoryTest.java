3e226b3a39995dba1c15672b2cfc93e1059315eb
이한솔 <hansol8701@naver.com>
2023-01-15 11:05:54 +0900

    test(Inquiry): 문의사항 엔티티 생성 및 테스트코드 작성

--- dabom-backend/src/test/java/joeuncamp/dabombackend/domain/post/repository/PostRepositoryTest.java
+++ dabom-backend/src/test/java/joeuncamp/dabombackend/domain/post/repository/PostRepositoryTest.java
@@ -4,6 +4,7 @@ import joeuncamp.dabombackend.domain.course.entity.Course;
 import joeuncamp.dabombackend.domain.course.repository.CourseJpaRepository;
 import joeuncamp.dabombackend.domain.member.entity.Member;
 import joeuncamp.dabombackend.domain.member.repository.MemberJpaRepository;
+import joeuncamp.dabombackend.domain.post.entity.Inquiry;
 import joeuncamp.dabombackend.domain.post.entity.Review;
 import joeuncamp.dabombackend.global.config.JpaAuditingConfig;
 import org.junit.jupiter.api.DisplayName;
@@ -22,7 +23,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class PostRepositoryTest {
     @Autowired
-    PostJpaRepository<Review> postJpaRepository;
+    PostJpaRepository<Review> reviewJpaRepository;
+
+    @Autowired
+    PostJpaRepository<Inquiry> inquiryJpaRepository;
 
     @Autowired
     MemberJpaRepository memberJpaRepository;
@@ -39,12 +43,11 @@ public class PostRepositoryTest {
         Review review = Review.builder()
                 .member(member)
                 .course(course)
-                .likes(15)
                 .build();
 
         // when
-        Review saved = postJpaRepository.save(review);
-        Review found = postJpaRepository.findById(saved.getId()).orElse(null);
+        Review saved = reviewJpaRepository.save(review);
+        Review found = reviewJpaRepository.findById(saved.getId()).orElse(null);
 
         // then
         assertThat(found).isNotNull();
@@ -52,7 +55,7 @@ public class PostRepositoryTest {
 
     @Test
     @DisplayName("후기를 저장하면, 작성자와 강좌와 연관관계가 맺어진다")
-    void 작성자와_강좌와_연관관계가_맺어진다() {
+    void 후기는_작성자와_강좌와_연관관계가_맺어진다() {
         // given
         Member member = Member.builder().build();
         Course course = Course.builder().build();
@@ -62,11 +65,10 @@ public class PostRepositoryTest {
         Review review = Review.builder()
                 .member(member)
                 .course(course)
-                .likes(15)
                 .build();
 
         // when
-        Review saved = postJpaRepository.save(review);
+        Review saved = reviewJpaRepository.save(review);
 
         // then
         assertThat(saved.getMember()).isEqualTo(member);
@@ -74,4 +76,49 @@ public class PostRepositoryTest {
         assertThat(member.getPostList()).contains(saved);
         assertThat(course.getPostList()).contains(saved);
     }
+
+    @Test
+    @DisplayName("문의사항를 저장하고 조회한다")
+    void 문의사항을_저장하고_조회한다() {
+        // given
+        Member member = Member.builder().build();
+        Course course = Course.builder().build();
+        Inquiry inquiry = Inquiry.builder()
+                .member(member)
+                .course(course)
+                .build();
+
+        // when
+        Inquiry saved = inquiryJpaRepository.save(inquiry);
+        Inquiry found = inquiryJpaRepository.findById(saved.getId()).orElse(null);
+
+        // then
+        assertThat(found).isNotNull();
+    }
+
+    @Test
+    @DisplayName("문의사항을 저장하면, 작성자와 강좌와 연관관계가 맺어진다")
+    void 문의사항은_작성자와_강좌와_연관관계가_맺어진다() {
+        // given
+        Member member = Member.builder().build();
+        Course course = Course.builder().build();
+        memberJpaRepository.save(member);
+        courseJpaRepository.save(course);
+
+        Inquiry inquiry = Inquiry.builder()
+                .member(member)
+                .course(course)
+                .build();
+
+        // when
+        Inquiry saved = inquiryJpaRepository.save(inquiry);
+
+        // then
+        assertThat(saved.getMember()).isEqualTo(member);
+        assertThat(saved.getCourse()).isEqualTo(course);
+        assertThat(member.getPostList()).contains(saved);
+        assertThat(course.getPostList()).contains(saved);
+    }
+
+
 }
