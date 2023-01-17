ecbc9a3a1ad96835fc30346be700bd9902c8718d
Sim <zh025700@naver.com>
2023-01-17 12:57:16 +0900

    feat: 깃허브 에러 탐지 중

--- frontend/src/components/account/SignIn.jsx
+++ frontend/src/components/account/SignIn.jsx
@@ -73,7 +73,7 @@ export default function SignIn() {
       alert("Ss");
     } catch {}
   };
-  console.log("eees");
+  console.log("eeesss");
   return (
     <AccountWrapper>
       <AccountTitle>로그인</AccountTitle>
