d19f595c79d40e1381cc6bbe2d33e3e906940843
jaeheon <78261574+Jaeheon-Sim@users.noreply.github.com>
2023-01-17 17:33:21 +0900

    Frontend (#47)
    * feat: 0112 UI 완성 및 카테고리바 아코디언 및 리액트 훅 폼 적용 중
    
    * feat: 폼에 관해 react-hook-form 적용
    
    - 정규표현식은 적용 안함
    - 단순 valid 및 onblur에 관한 valid check
    - input 컴포넌트들 구조 최적화
    
    * feat: form button에 disable 적용 보류
    
    * feat: 플레이어 이식
    
    * test: 브랜치 테스트
    
    * feat: 플레이어 반응형 사이드바 구현
    
    * chore: 사이드바 리팩토링
    
    - 플레이어 컨트롤러 아이콘 크기 조정
    - 플레이어 레이아웃 조정
    - 플레이어 리뷰 섹션 레이아웃 조정
    
    * test: 충돌 방지 테스트
    
    * test: git revert 결과 확인
    
    * test: 깃허브 에러 탐지 중
    
    * feat: 깃허브 에러 탐지 중
    
    * test: 프론트엔드 브랜치 원상 복구
    
    * feat: 카카오 로그인 적용 중
    - 리액트 쿼리 post 도입
    - api 코드 작성 중

--- frontend/src/api/socialAuthApi.js
+++ frontend/src/api/socialAuthApi.js
@@ -1,6 +1,26 @@
 import axios from "axios";
+import { KAKAO_CLIENT_ID, KAKAO_REDIRECT_URL } from "../AuthKey";
 
 const BASE_URL = "https://api.coinpaprika.com/v1";
+const KAKAO_URL = "https://kauth.kakao.com/oauth/token";
 export async function fetchCoins() {
   return axios.get(`${BASE_URL}/coins`);
+}
+
+const kakoApi = axios.create({
+  baseURL: KAKAO_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Origin": "*",
+  },
+});
+
+export async function getKakaoAuthToken(code) {
+  return kakoApi.post({
+    grant_type: "authorization_code",
+    client_id: KAKAO_CLIENT_ID,
+    redirect_uri: KAKAO_REDIRECT_URL,
+    code: code,
+  });
 }
--- frontend/src/components/account/KakaoAuth.jsx
+++ frontend/src/components/account/KakaoAuth.jsx
@@ -1,15 +1,24 @@
-import { fetchCoins } from "../../api/socialAuthApi";
-import { useQuery } from "react-query";
+import { fetchCoins, getKakaoAuthToken } from "../../api/socialAuthApi";
+import { useMutation, useQuery } from "react-query";
 import { Suspense, useEffect } from "react";
+import { useParams } from "react-router";
 
 export default function KaKaoAuth() {
-  const { status, data, error, isIdle } = useQuery("test", fetchCoins, {
-    suspense: true,
-  });
+  const kakao = useMutation(getKakaoAuthToken.post);
+  // const { status, data, error, isIdle } = useMutation(
+  //   "test",
+  //   getKakaoAuthToken.post,
+  //   {
+  //     suspense: true,
+  //   }
+  // );
 
-  console.log(data);
-  console.log(isIdle);
+  const code = new URL(window.location.href).searchParams.get("code");
 
+  useEffect(() => {
+    kakao.mutate({ code });
+  }, [code]);
+  console.log(kakao);
   return (
     <Suspense fallback={<div>loading</div>}>
       <div>실험중</div>
