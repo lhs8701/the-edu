c3924a9e33959a67e6b975cf8a1b7f7fe8025551
kth1210 <k2hop1210@gmail.com>
2023-01-18 14:33:52 +0900

    [CHORE]: URL Constant 추가

--- dabom_iOS/dabom_iOS/Constants/Url.swift
+++ dabom_iOS/dabom_iOS/Constants/Url.swift
@@ -11,8 +11,21 @@ extension Const {
     struct Url {
         
         static let serverIP = "218.38.127.26:8080"
-        static let getCourseInfo = "http://\(serverIP)/api/courses/"
         
+        // MARK: - Course
+        static let getCourseInfo = "http://\(serverIP)/api/courses/"
+        static let getCourseReview = "http://\(serverIP)/api/courses/"
+        static let getCourseInquiries = "http://\(serverIP)/api/courses/"
+        
+        static let getCategoryCourses = "http://\(serverIP)/api/courses/category/"
+        
+        // MARK: - Student
+        static let getMyCourses = "http://\(serverIP)/api/students/"
+        static let getMyWishCourses = "http://\(serverIP)/api/students/"
+        
+        // MARK: - Authentication
+        static let signup = "http://\(serverIP)/api/auth/basic/signup"
+        static let login = "http://\(serverIP)/api/auth/basic/login"
         
     }
 }
