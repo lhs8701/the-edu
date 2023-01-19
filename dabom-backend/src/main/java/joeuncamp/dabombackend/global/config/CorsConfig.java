3066cec869d1113931ad5a8fc9ef575e11f6b69e
이한솔 <hansol8701@naver.com>
2023-01-19 12:57:19 +0900

    cors 허용 설정

--- dabom-backend/src/main/java/joeuncamp/dabombackend/global/config/CorsConfig.java
+++ dabom-backend/src/main/java/joeuncamp/dabombackend/global/config/CorsConfig.java
@@ -7,12 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
-                .allowedHeaders("headers")
-                .maxAge(3000);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
+//                .allowedHeaders("headers")
+//                .maxAge(3000);
+//    }
 }
