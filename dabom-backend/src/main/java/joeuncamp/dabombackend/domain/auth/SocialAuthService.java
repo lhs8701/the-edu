816fb8348fb25df573678710a3d79f9171f65333
이한솔 <hansol8701@naver.com>
2023-01-19 10:41:56 +0900

    feat(auth): 카카오 계정으로 회원가입/로그인/로그아웃하는 기능 추가

--- dabom-backend/src/main/java/joeuncamp/dabombackend/domain/auth/SocialAuthService.java
+++ dabom-backend/src/main/java/joeuncamp/dabombackend/domain/auth/SocialAuthService.java
@@ -5,7 +5,7 @@ import joeuncamp.dabombackend.global.security.jwt.TokenForm;
 public interface SocialAuthService {
     public TokenForm login(String socialToken);
 
-    public void logout(String kakaoToken, String socialToken);
+    public void logout(String socialToken, String accessToken);
 
-    public void withdraw(String accessToken);
+    public void withdraw(String socialToken, String accessToken);
 }
--- dabom-backend/src/main/java/joeuncamp/dabombackend/domain/auth/kakaoAuthService.java
+++ dabom-backend/src/main/java/joeuncamp/dabombackend/domain/auth/kakaoAuthService.java
@@ -1,11 +1,8 @@
 package joeuncamp.dabombackend.domain.auth;
 
-import joeuncamp.dabombackend.domain.auth.basic.dto.SignupRequestDto;
 import joeuncamp.dabombackend.domain.member.entity.Member;
 import joeuncamp.dabombackend.domain.member.repository.MemberJpaRepository;
 import joeuncamp.dabombackend.global.constant.LoginType;
-import joeuncamp.dabombackend.global.error.exception.CMemberExistException;
-import joeuncamp.dabombackend.global.error.exception.CResourceNotFoundException;
 import joeuncamp.dabombackend.global.security.jwt.JwtProvider;
 import joeuncamp.dabombackend.global.security.jwt.TokenForm;
 import joeuncamp.dabombackend.util.kakaoapi.KakaoService;
@@ -17,17 +14,25 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class kakaoAuthService implements SocialAuthService{
+public class kakaoAuthService implements SocialAuthService {
 
     private final JwtProvider jwtProvider;
     private final KakaoService kakaoService;
     private final MemberJpaRepository memberJpaRepository;
 
+    /**
+     * 카카오 토큰으로 로그인합니다.
+     * 카카오 계정의 아이디와 '카카오'로그인 타입을 파라미터로 DB에서 회원을 조회한 후, 없으면 회원을 생성합니다.
+     *
+     * @param kakaoToken 카카오에서 발급한 토큰
+     * @return 어세스토큰, 리프레시 토큰
+     */
+    @Override
     public TokenForm login(String kakaoToken) {
         KakaoProfile profile = kakaoService.getKakaoProfile(kakaoToken);
         String kakaoId = String.valueOf(profile.getId());
         Optional<Member> found = memberJpaRepository.findByLoginTypeAndSocialId(LoginType.KAKAO, kakaoId);
-        if (found.isEmpty()){
+        if (found.isEmpty()) {
             Member created = signup(profile);
             return jwtProvider.generateToken(created);
         }
@@ -39,12 +44,31 @@ public class kakaoAuthService implements SocialAuthService{
         return memberJpaRepository.save(member);
     }
 
+    /**
+     * 카카오 토큰으로 로그아웃합니다.
+     * 카카오 로그아웃 API를 호출한 후, 어세스토큰과 리프레시토큰을 만료시킵니다.
+     *
+     * @param kakaoToken  카카오에서 발급한 토큰
+     * @param accessToken 어세스토큰
+     */
+    @Override
     public void logout(String kakaoToken, String accessToken) {
         kakaoService.logout(kakaoToken);
         /* +어세스 토큰 만료 로직 */
     }
 
-    public void withdraw(String accessToken) {
+    /**
+     * 계정을 탈퇴합니다.
+     * 카카오 Unlink API를 호출한 후, 어세스토큰과 리프레시토큰을 만료시킵니다.
+     * DB에서 회원을 삭제합니다.
+     *
+     * @param kakaoToken  카카오에서 발급한 토큰
+     * @param accessToken 어세스토큰
+     */
+    @Override
+    public void withdraw(String kakaoToken, String accessToken) {
+        kakaoService.unlink(kakaoToken);
+        /* +어세스 토큰 만료 로직 */
         Member member = (Member) jwtProvider.getAuthentication(accessToken).getPrincipal();
         memberJpaRepository.deleteById(member.getId());
     }
