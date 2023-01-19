c662b2a953085233e07b753e83fcc0b8f5515e1f
Sim <zh025700@naver.com>
2023-01-19 09:48:08 +0900

    feat:강의 intro api 연결

--- frontend/src/pages/coursePage.jsx
+++ frontend/src/pages/coursePage.jsx
@@ -1,5 +1,8 @@
+import { Suspense } from "react";
 import { useQueries, useQuery } from "react-query";
+import { useParams } from "react-router";
 import styled from "styled-components";
+import { courseApi } from "../api/courseApi";
 import CourseDetail from "../components/course/CourseDetail";
 import CourseIntro from "../components/course/CourseIntro";
 import CoursePayment from "../components/course/CoursePayment";
@@ -12,19 +15,34 @@ const DividerBox = styled.div`
 `;
 
 export default function CoursePage() {
-  const courseInfo = dummyCourseInfo;
-  // const { data, isLoading, isError } = useQuery();
+  // const courseInfo = dummyCourseInfo;
+  // const { courseId } = useParams();
+  const courseId = 1;
+  const courseInfo = useQuery(
+    ["courseDetailInfo", courseId],
+    () => {
+      return courseApi(courseId);
+    },
+    {
+      enabled: !!courseId,
+      retry: 1,
+      // onSuccess: (data) => {},
+    }
+  );
+
   return (
     <div>
-      <CourseIntro courseInfo={courseInfo.courseInfo} />
-      <DividerBox>
-        <CourseDetail courseInfo={courseInfo} />
-        <CoursePayment
-          title={courseInfo?.courseInfo?.title}
-          teacher={courseInfo?.courseInfo?.teacher}
-          purchaseOption={courseInfo.coursePurchaseInfo}
-        />
-      </DividerBox>
+      <Suspense fallback={<div>로딩중</div>}>
+        <CourseIntro courseInfo={courseInfo.data} />
+        {/* <DividerBox>
+          <CourseDetail courseInfo={courseInfo.data} />
+          <CoursePayment
+            title={courseInfo?.data?.title}
+            teacher={courseInfo?.data?.instructor}
+            purchaseOption={courseInfo.data}
+          />
+        </DividerBox> */}
+      </Suspense>
     </div>
   );
 }
