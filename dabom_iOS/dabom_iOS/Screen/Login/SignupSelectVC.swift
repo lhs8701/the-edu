5c9fe113cdd9b703aa73f169e06d1df538883c38
kth1210 <k2hop1210@gmail.com>
2023-01-19 15:54:06 +0900

    [FEAT]: AppleLogin 구현(로컬)

--- dabom_iOS/dabom_iOS/Screen/Login/SignupSelectVC.swift
+++ dabom_iOS/dabom_iOS/Screen/Login/SignupSelectVC.swift
@@ -8,18 +8,19 @@
 import UIKit
 import KakaoSDKAuth
 import KakaoSDKUser
+import AuthenticationServices
 
 class SignupSelectVC: UIViewController {
     
     var rootView: LoginSignupVC?
-
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-    
+    // MARK: - 이메일 회원가입
     @IBAction func emailSignupBtnPressed(_ sender: Any) {
         self.dismiss(animated: true) {
             let nextVC = UIStoryboard(name: Const.Storyboard.Name.loginSignup, bundle: nil).instantiateViewController(withIdentifier: Const.ViewController.Identifier.signup) as! SignupVC
@@ -28,6 +29,7 @@ class SignupSelectVC: UIViewController {
         }
     }
     
+    // MARK: - 카카오톡 회원가입
     @IBAction func kakaoSignup(_ sender: Any) {
         // 카카오톡 앱으로 로그인 가능하면
         if (UserApi.isKakaoTalkLoginAvailable()) {
@@ -57,22 +59,54 @@ class SignupSelectVC: UIViewController {
                 }
             }
         }
+    }
+    
+    // MARK: - Apple 회원가입
+    @IBAction func appleSignup(_ sender: Any) {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
         
-        
-//        UserApi.shared.loginWithKakaoAccount { (oauthToken, error) in
-//            if let error = error {
-//                print(error)
-//            } else {
-//                print("loginWithKakaoAccount() success")
-//
-//                let accessToken = oauthToken?.accessToken
-//                let refreshToken = oauthToken?.refreshToken
-//                print(String(accessToken ?? ""))
-//                print(String(refreshToken ?? ""))
-//            }
-//        }
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
         
     }
     
+
+}
+
+
+extension SignupSelectVC: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+}
+
+extension SignupSelectVC: ASAuthorizationControllerDelegate {
+    // MARK: - Apple 인증 성공 시
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            let userIdentifier = appleIDCredential.user
+            let userName = (appleIDCredential.fullName?.familyName ?? "") + (appleIDCredential.fullName?.givenName ?? "")
+            if let email = appleIDCredential.email {
+                print(email)
+            }
+            print(userIdentifier)
+            print(userName)
+            
+            
+        }
+    }
     
+    // MARK: - Apple 인증 실패 시
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("authorization Error: \(error)")
+        let alert = UIAlertController(title: "알림", message: "Apple 로그인 실패", preferredStyle: UIAlertController.Style.alert)
+        let confirm = UIAlertAction(title: "확인", style: UIAlertAction.Style.cancel, handler: nil)
+        
+        alert.addAction(confirm)
+        self.present(alert, animated: true)
+    }
 }
