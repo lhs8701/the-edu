563b5ab1c9cdf7ca429f31b8dce32c82c8848402
이한솔 <hansol8701@naver.com>
2023-01-19 15:41:22 +0900

    test

---
+++ dabom-backend/src/main/java/joeuncamp/dabombackend/global/constant/test.java
@@ -0,0 +1,30 @@
+package joeuncamp.dabombackend.global.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@AllArgsConstructor
+@Getter
+public enum test {
+    BACK_END("백엔드"),
+    FRONT_END("프론트엔드"),
+    DATA_PROCESSING("정보처리 자격증"),
+    ELECTRICAL("전기 자격증"),
+    TOEIC("토익"),
+    TOEFL("토플"),
+    STOCK("주식"),
+    FUND("펀드"),
+    EMPTY("없음");
+
+
+    private final String title;
+
+    public static test findByTitle(String title){
+        return Arrays.stream(values())
+                .filter(type -> type.getTitle().equals(title))
+                .findAny()
+                .orElse(EMPTY);
+    }
+}
