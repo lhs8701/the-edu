791b0699c3f9848ac4c127ad9296f2da7c9f4e2e
심재헌 <simjaeheon@simjaeheons-MacBook-Air.local>
2023-02-14 11:30:06 +0900

    feat: 비밀번호 리다이렉트 추가

--- frontend/src/components/account/FindPassword.jsx
+++ frontend/src/components/account/FindPassword.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { PROCESS_ACCOUNT_URL } from "../../static";
 import {
@@ -14,6 +14,7 @@ import {
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { resetPwdApi } from "../../api/myPageApi";
+import { CircularProgress } from "@mui/material";
 
 const LoginLinkBox = styled.div`
   width: 100%;
@@ -53,6 +54,8 @@ const Div = styled.div`
 export default function FindPassword() {
   const [isID, setIsId] = useState("");
   const [isName, setIsName] = useState("");
+  const [isSubmit, setIsSubmit] = useState(false);
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -60,59 +63,70 @@ export default function FindPassword() {
   } = useForm();
 
   const submit = () => {
+    setIsSubmit(true);
     resetPwdApi(isID).then(({ data }) => {
       alert(data.email + "로 임시 비밀번호를 보냈어요");
+      navigate(-1);
     });
   };
 
   return (
     <AccountWrapper>
       <AccountTitle>비밀번호 찾기</AccountTitle>
-      <AccountForm onSubmit={handleSubmit(submit)}>
-        <InputBox>
-          <InputLabel>아이디</InputLabel>
-          <AccountInput
-            {...register("email", {
-              name: "email",
-              required: "아이디를 입력하세요!",
-              // 유효성 검사 파트
-              // pattern: {
-              //   value: /^[A-Za-z0-9._%+-]+@knu\.ac.kr$/,
-              //   message: "wrong input",
-              // },
-              onChange: (e) => {
-                setIsId(e.target.value);
-              },
-            })}
-            type="email"
-            placeholder="아이디를 입력해주세요."
-          />
-          <ErrorMessage>{errors?.email?.message}</ErrorMessage>
-        </InputBox>
-        <InputBox>
-          <InputLabel>성명</InputLabel>
-          <AccountInput
-            {...register("name", {
-              name: "name",
-              required: "이름을 입력하세요!",
-              // 유효성 검사 파트
-              // pattern: {
-              //   value: /^[A-Za-z0-9._%+-]+@knu\.ac.kr$/,
-              //   message: "wrong input",
-              // },
-              onChange: (e) => {
-                setIsName(e.target.value);
-              },
-            })}
-            type="text"
-            placeholder="이름을 입력해주세요."
-          />
-          <ErrorMessage>{errors?.name?.message}</ErrorMessage>
-        </InputBox>
-        <LoginLinkBox>
-          <Btn>비밀번호 찾기</Btn>
-        </LoginLinkBox>
-      </AccountForm>
+      {!isSubmit ? (
+        <AccountForm onSubmit={handleSubmit(submit)}>
+          <InputBox>
+            <InputLabel>아이디</InputLabel>
+            <AccountInput
+              {...register("email", {
+                name: "email",
+                required: "아이디를 입력하세요!",
+                // 유효성 검사 파트
+                // pattern: {
+                //   value: /^[A-Za-z0-9._%+-]+@knu\.ac.kr$/,
+                //   message: "wrong input",
+                // },
+                onChange: (e) => {
+                  setIsId(e.target.value);
+                },
+              })}
+              type="email"
+              placeholder="아이디를 입력해주세요."
+            />
+            <ErrorMessage>{errors?.email?.message}</ErrorMessage>
+          </InputBox>
+          <InputBox>
+            <InputLabel>성명</InputLabel>
+            <AccountInput
+              {...register("name", {
+                name: "name",
+                required: "이름을 입력하세요!",
+                // 유효성 검사 파트
+                // pattern: {
+                //   value: /^[A-Za-z0-9._%+-]+@knu\.ac.kr$/,
+                //   message: "wrong input",
+                // },
+                onChange: (e) => {
+                  setIsName(e.target.value);
+                },
+              })}
+              type="text"
+              placeholder="이름을 입력해주세요."
+            />
+            <ErrorMessage>{errors?.name?.message}</ErrorMessage>
+          </InputBox>
+          <LoginLinkBox>
+            <Btn>비밀번호 찾기</Btn>
+          </LoginLinkBox>
+        </AccountForm>
+      ) : (
+        <CircularProgress
+          sx={{
+            color: "var(--color-primary)",
+          }}
+        />
+      )}
+
       <br />
 
       <Div>
