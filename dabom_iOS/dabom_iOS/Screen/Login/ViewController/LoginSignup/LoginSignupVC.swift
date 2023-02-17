b2177c77ff4a9d5804a6d08851feca13629678aa
kth1210 <k2hop1210@gmail.com>
2023-02-17 15:49:01 +0900

    [CHORE] : LoginSign - 코드 수정, mark 추가

--- dabom_iOS/dabom_iOS/Screen/Login/ViewController/LoginSignup/LoginSignupVC.swift
+++ dabom_iOS/dabom_iOS/Screen/Login/ViewController/LoginSignup/LoginSignupVC.swift
@@ -9,23 +9,38 @@ import UIKit
 
 class LoginSignupVC: UIViewController {
 
-    @IBOutlet weak var signupSelectBtn: UIButton!
+    // MARK: - IBOutlet
     
+    @IBOutlet weak var signupSelectBtn: UIButton!
     @IBOutlet weak var loginSelectBtn: UIButton!
+    
+    
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setNavi()
+        configureView()
+    }
+    
+    
+    // MARK: - func
+    
+    private func setNavi() {
         self.navigationController?.navigationBar.topItem?.backButtonTitle = ""
         self.navigationController?.navigationBar.tintColor = .black
-        
+    }
+    
+    private func configureView() {
         [signupSelectBtn, loginSelectBtn].forEach {
             $0?.layer.cornerRadius = 10
         }
     }
     
-
+    
+    // MARK: - 신규 회원 가입 버튼 클릭 시
+    
     @available(iOS 15.0, *)
     @IBAction func signupBtnPressed(_ sender: Any) {
         guard let selectVC = UIStoryboard(name: Const.Storyboard.Name.loginSignup, bundle: nil).instantiateViewController(withIdentifier: Const.ViewController.Identifier.signupSelect) as? SignupSelectVC else {return}
@@ -34,9 +49,6 @@ class LoginSignupVC: UIViewController {
         
         if let sheet = selectVC.sheetPresentationController {
             sheet.detents = [.medium()]
-            
-//            sheet.delegate = self
-            
             sheet.prefersGrabberVisible = true
         }
         
@@ -44,6 +56,8 @@ class LoginSignupVC: UIViewController {
         present(selectVC, animated: true, completion: nil)
     }
     
+    
+    // MARK: - 기존 회원 로그인 버튼 클릭 시
     
     @IBAction func loginBtnPressed(_ sender: Any) {
         guard let loginVC = UIStoryboard(name: Const.Storyboard.Name.loginSignup, bundle: nil).instantiateViewController(withIdentifier: Const.ViewController.Identifier.login) as? LoginVC else {return}
