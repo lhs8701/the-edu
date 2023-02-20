123ce85a83bc9405b311c9d65d762cfdfa83d460
kth1210 <k2hop1210@gmail.com>
2023-02-20 09:33:39 +0900

    [REFACTOR] : CourseInfo Cell 코드 수정

--- dabom_iOS/dabom_iOS/Screen/Course/Cell/CourseInfo/CourseCurriculumTVC.swift
+++ dabom_iOS/dabom_iOS/Screen/Course/Cell/CourseInfo/CourseCurriculumTVC.swift
@@ -7,18 +7,25 @@
 
 import UIKit
 
+// MARK: - 커리큘럼 보기 버튼 Delegate 패턴
 protocol allCurriculumBtnDelegate {
     func allCurriculumBtnPressed()
 }
 
 class CourseCurriculumTVC: UITableViewCell {
 
+    // MARK: - IBOutlet
     
     @IBOutlet weak var curriculumTitle: UILabel!
-    
     @IBOutlet weak var allCurriculumBtn: UIButton!
     
+    
+    // MARK: - let, var
+    
     var delegate: allCurriculumBtnDelegate?
+    
+    
+    // MARK: - Life Cycle
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -31,11 +38,13 @@ class CourseCurriculumTVC: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
     
+    
+    // MARK: - 커리큘럼 보기 버튼 눌렀을 때 Delegate 패턴
+    
     @IBAction func allCurriculumBtnPressed(_ sender: Any) {
-        
         delegate?.allCurriculumBtnPressed()
     }
+    
 }
