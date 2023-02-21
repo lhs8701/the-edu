39cf46d508d4cef1370c814221eb3e8d3acb7e82
심재헌 <simjaeheon@simjaeheons-MacBook-Air.local>
2023-02-21 09:24:10 +0900

    chore: 빌드용 업로드 - 상수 변경

--- frontend/src/purchase.js
+++ frontend/src/purchase.js
@@ -1,6 +1,7 @@
 import { loadTossPayments } from "@tosspayments/payment-sdk";
 import { TOSS_CLIENTKEY } from "./AuthKey";
 import uuid from "react-uuid";
+import { TOSS_PURCHASE_URL } from "./static";
 
 export function purchase(purchaseInfo, method) {
   loadTossPayments(TOSS_CLIENTKEY).then((tossPayments) => {
@@ -10,8 +11,8 @@ export function purchase(purchaseInfo, method) {
         orderId: String(uuid()),
         orderName: String(purchaseInfo.orderName),
         customerName: String(purchaseInfo.customerName),
-        successUrl: `http://localhost:3000/purchase/${purchaseInfo.courseId}/${purchaseInfo.itemId}/${purchaseInfo.point}/${purchaseInfo.couponId}/success`,
-        failUrl: `http://localhost:3000/purchase/${purchaseInfo.courseId}/${purchaseInfo.itemId}/${purchaseInfo.point}/${purchaseInfo.couponId}/fail`,
+        successUrl: `${TOSS_PURCHASE_URL}/${purchaseInfo.courseId}/${purchaseInfo.itemId}/${purchaseInfo.point}/${purchaseInfo.couponId}/success`,
+        failUrl: `${TOSS_PURCHASE_URL}/${purchaseInfo.courseId}/${purchaseInfo.itemId}/${purchaseInfo.point}/${purchaseInfo.couponId}/fail`,
       })
       .catch((error) => {
         if (error.code === "USER_CANCEL") {
