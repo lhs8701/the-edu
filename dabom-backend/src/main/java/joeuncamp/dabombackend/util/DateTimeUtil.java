10c13be35ef91a27ab2f49decdf83d68da865afd
이한솔 <hansol8701@naver.com>
2023-02-22 14:53:41 +0900

    test: DateTimeUtil 테스트

--- dabom-backend/src/main/java/joeuncamp/dabombackend/util/DateTimeUtil.java
+++ dabom-backend/src/main/java/joeuncamp/dabombackend/util/DateTimeUtil.java
@@ -6,6 +6,10 @@ import org.springframework.cglib.core.Local;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class DateTimeUtil {
@@ -14,16 +18,25 @@ public class DateTimeUtil {
         LocalDateTime createdTime = LocalDateTime.of(2020, 10, 9, 12, 12, 20);
         LocalDate memberJoinedDate = createdTime.toLocalDate();
         LocalDate startDate = YearMonth.from(memberJoinedDate).atDay(1);
-        LocalDate endDate = YearMonth.from(startDate).atEndOfMonth();
-        while (startDate.getYear() <= LocalDate.now().getYear()){
-
-        }
+        LocalDate endDate = YearMonth.from(memberJoinedDate).atEndOfMonth();
+        Map<Integer, List<LocalDate>> map = new HashMap<>();
         while (startDate.isBefore(LocalDate.now())) {
-            log.info("startDate:{}", startDate);
-            log.info("endDate:{}", endDate);
+            if (!map.containsKey(startDate.getYear())){
+                map.put(startDate.getYear(), new ArrayList<>());
+            }
+            /* 로직 시작*/
+            //
+            map.get(startDate.getYear()).add(startDate);
+            map.get(endDate.getYear()).add(endDate);
+            /* 로직 끝 */
             startDate = startDate.plusMonths(1);
             endDate = YearMonth.from(startDate).atEndOfMonth();
         }
-
+        for (List<LocalDate> value : map.values()) {
+            log.info("-----------------");
+            for (LocalDate localDate : value) {
+                log.info("{}", localDate);
+            }
+        }
     }
 }
